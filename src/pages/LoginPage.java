276a980bdec8ab62e6894dc0d11502a92b099839
nenad97ng <nenad97ng@gmail.com>
2020-09-29 15:07:20 +0200

    Created LoginPage.

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasicPage {
+
+	public LoginPage(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+	}
+
+	// Getters for all elements
+	public WebElement getEmail() {
+		return driver.findElement(By.xpath("//*[@id=\"frm_fat_id_frmLogin\"]/fieldset[1]/input"));
+	}
+
+	public WebElement getPassword() {
+		return driver.findElement(By.xpath("//*[@id=\"frm_fat_id_frmLogin\"]/fieldset[2]/input"));
+	}
+
+	public WebElement getLoginSubmit() {
+		return driver.findElement(By.xpath("//*[@id=\"frm_fat_id_frmLogin\"]/fieldset[4]/input"));
+	}
+
+	// Method for login
+	public void login(String email, String password) {
+		this.getEmail().sendKeys(email);
+		this.getPassword().sendKeys(password);
+		this.getLoginSubmit().click();
+	}
+}
