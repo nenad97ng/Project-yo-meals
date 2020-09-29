2fc86c9059e7638d7f5e8f095b4fa752b25f22d3
nenad97ng <nenad97ng@gmail.com>
2020-09-29 13:34:07 +0200

    Creating LocationPopupPage.

---
+++ src/pages/LocationPopupPage.java
@@ -0,0 +1,57 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LocationPopupPage extends BasicPage {
+
+	public LocationPopupPage(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+	}
+
+	// Get metods for get Popup and close
+	public WebElement getSelectLocation() {
+		return driver.findElement(By.xpath("//*[@id=\"header\"]/div[1]/div/div/div[1]/div/a"));
+	}
+
+	public WebElement getPopupCloseElement() {
+		return driver.findElement(By.xpath("//*[@id=\"location-popup\"]/div/div/div/div/a"));
+	}
+
+	// Get metods for location inputs
+	public WebElement getKeyword() {
+		return driver.findElement(By.xpath("//*[@id='locality_keyword']"));
+	}
+
+	public WebElement getLocationItem(String locationName) {
+		return driver.findElement(By.xpath("//li/a[contains(text(), '" + locationName + "')]/.."));
+	}
+
+	public WebElement getLocationInput() {
+		return driver.findElement(By.xpath("//*[@id='location_id']"));
+	}
+
+	public WebElement getSubmit() {
+		return driver.findElement(By.xpath("//*[@name='btn_submit']"));
+	}
+
+	// Metods for open and set location
+	public void openPopup() {
+		this.getSelectLocation().click();
+	}
+
+	public void setLocation(String locationName) {
+		this.getKeyword().click();
+
+		String dataValue = this.getLocationItem(locationName).getAttribute("data-value");
+
+		js.executeScript("arguments[0].value=arguments[1];", this.getLocationInput(), dataValue);
+		js.executeScript("arguments[0].click();", this.getSubmit());
+	}
+
+	public void closePopup() {
+		this.getPopupCloseElement().click();
+	}
+}
