e8d9f62911c1136809afa8375cdb348a151d4af3
nenad97ng <nenad97ng@gmail.com>
2020-10-01 18:13:35 +0200

    Update-Changed path for getAddToFavorite Method.

--- src/pages/MealPage.java
+++ src/pages/MealPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,11 +21,12 @@ public class MealPage extends BasicPage{
 		return driver.findElement(By.xpath("//*[@id=\"body\"]/section[1]/div/div/div[2]/div/div[3]/div[2]/a"));
 	}
 	public WebElement getAddToFavorite() {
-		return driver.findElement(By.xpath("//*[@id=\"item_119\"]"));
+		return driver.findElement(By.xpath("/html/body/div[6]/section[1]/div/div/div[1]/div[1]/a"));
 	}
 	
 	//Cart and Favorite methods
 	public void addMealToCart(String quantity) {
+		this.getQtyInput().sendKeys(Keys.CONTROL + "a", Keys.DELETE);
 		this.getQtyInput().sendKeys(quantity);
 		js.executeScript("arguments[0].click();", this.getAddToCart());
 	}
