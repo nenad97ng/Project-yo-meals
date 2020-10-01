12cd2c82b29193fc87d4060b00f1c96c48ccee8e
nenad97ng <nenad97ng@gmail.com>
2020-10-01 19:37:06 +0200

    Update-Edit Asserts.

--- src/tests/MealItemTest.java
+++ src/tests/MealItemTest.java
@@ -23,114 +23,111 @@ import pages.ProfilePage;
 
 public class MealItemTest extends BasicTest {
 
-	
-	@Test(priority=1)
+	@Test(priority = 1)
 	public void addMealToCartTest() throws InterruptedException {
 		this.driver.navigate().to(this.baseUrl + "/meal/lobster-shrimp-chicken-quesadilla-combo");
-		
+
+		// Import Pages
 		LocationPopupPage locationPopup = new LocationPopupPage(driver, wait, js);
 		NotificationSystemPage notificationSystem = new NotificationSystemPage(driver, wait, js);
 		MealPage meal = new MealPage(driver, wait, js);
-		
+
+		// Add meal to cart
 		locationPopup.closePopup();
 		meal.addMealToCart("3");
-		Assert.assertTrue(notificationSystem.getMessage().contains("The Following Errors Occurred:"),"[ERROR]: The Following Errors Occurred:");
-		Assert.assertTrue(notificationSystem.getMessage().contains("Please Select Location"), "[ERROR]: Please Select Location");
-		
+		Assert.assertTrue(notificationSystem.getMessage().contains("The Following Errors Occurred:"),
+				"[ERROR]: Error message was not displayed.");
+		Assert.assertTrue(notificationSystem.getMessage().contains("Please Select Location"),
+				"[ERROR]: Please Select Location message was not displayed.");
+
 		notificationSystem.notificationInvisible();
-		
+
+		// Set location
 		locationPopup.openPopup();
 		locationPopup.setLocation("City Center - Albany");
-		
+
+		// Add meal to cart
 		Thread.sleep(2000);
 		meal.addMealToCart("3");
-		
-		Assert.assertTrue(notificationSystem.getMessage().contains("Meal Added To Cart"), "[ERROR]: Meal not added to cart.");
 
-		
+		Assert.assertTrue(notificationSystem.getMessage().contains("Meal Added To Cart"),
+				"[ERROR]: Meal Added message was not displayed.");
 	}
-	@Test(priority=2)
-	public void addMealToFavoriteTest() throws InterruptedException {
-		Thread.sleep(5000);
-		this.driver.navigate().to(this.baseUrl + "/meal/lobster-shrimp-chicken-quesadilla-combo");
-		
 
+	@Test(priority = 2)
+	public void addMealToFavoriteTest() throws InterruptedException {
+		Thread.sleep(2000);
+		this.driver.navigate().to(this.baseUrl + "/meal/lobster-shrimp-chicken-quesadilla-combo");
+
+		// Import Pages
+		LocationPopupPage locationPopup = new LocationPopupPage(driver, wait, js);
 		LoginPage login = new LoginPage(driver, wait, js);
 		NotificationSystemPage notificationSystem = new NotificationSystemPage(driver, wait, js);
 		MealPage meal = new MealPage(driver, wait, js);
 
-		
-		
-
+		// Add meal to favorite
+		locationPopup.closePopup();
 		Thread.sleep(2000);
 		meal.addMealToFavorite();
-		
-		
-		Assert.assertTrue(notificationSystem.getMessage().contains("Please login first!"));
-		
+
+		Assert.assertTrue(notificationSystem.getMessage().contains("Please login first!"),
+				"[ERROR]:Please login message was not displayed.");
+
 		this.driver.navigate().to(this.baseUrl + "/guest-user/login-form");
-		
+
+		// Login and add meal to favorite
 		login.login(email, password);
-		
+
 		this.driver.navigate().to(this.baseUrl + "/meal/lobster-shrimp-chicken-quesadilla-combo");
-		
+
 		meal.addMealToFavorite();
-		
-		Thread.sleep(1000);
-		Assert.assertTrue(notificationSystem.getMessage().contains("Product has been added to your favorites."), "[ERROR]: Message was not displayed.");
-
-		
-
-		
+		Thread.sleep(2000);
+		Assert.assertTrue(notificationSystem.getMessage().contains("Product has been added to your favorites."),
+				"[ERROR]:Product added message was not displayed.");
 
 	}
-	
-	@Test(priority=3)
+
+	@Test(priority = 3)
 	public void clearCartTest() throws InterruptedException, IOException {
-		Thread.sleep(5000);
+		Thread.sleep(2000);
 		this.driver.navigate().to(this.baseUrl + "/meals");
-	
+
+		// Import Pages
 		LocationPopupPage locationPopup = new LocationPopupPage(driver, wait, js);
 		NotificationSystemPage notificationSystem = new NotificationSystemPage(driver, wait, js);
 		MealPage meal = new MealPage(driver, wait, js);
 		CartSummaryPage cartSummary = new CartSummaryPage(driver, wait, js);
-		AuthPage auth = new AuthPage(driver, wait, js);
-		
+
 		SoftAssert sa = new SoftAssert();
-		
-		
-		auth.logout();
-		Thread.sleep(2000);
-		
-		this.driver.navigate().to(this.baseUrl + "/meals");
-		Thread.sleep(3000);
-		locationPopup.openPopup();
+
 		locationPopup.setLocation("City Center - Albany");
-		
-		
-		
+
+		// Import xlsx file
 		File file = new File("data/Data.xlsx");
 		FileInputStream fis = new FileInputStream(file);
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		XSSFSheet sheet = workbook.getSheet("Meals");
-		
+
+		// Add meal to cart (From xlsx file)
 		for (int i = 1; i < sheet.getLastRowNum() + 1; i++) {
 			XSSFRow row = sheet.getRow(i);
-			
+
 			XSSFCell meals = row.getCell(0);
 			String links = meals.getStringCellValue();
-			
-			this.driver.navigate().to(links);			
-			Thread.sleep(5000);
+
+			this.driver.navigate().to(links);
+			Thread.sleep(2000);
 			meal.addMealToCart("1");
-			
-			sa.assertTrue(notificationSystem.getMessage().contains("Meal Added To Cart"));
+
+			sa.assertTrue(notificationSystem.getMessage().contains("Meal Added To Cart"),
+					"[ERROR]:Meal added message was not displayed.");
 			Thread.sleep(1000);
 		}
 		sa.assertAll();
+		// Clear cart
 		cartSummary.clearAll();
-		
-		Assert.assertTrue(notificationSystem.getMessage().contains("All meals removed from Cart successfully"), "[ERROR]: Meals not removed.");
-		Thread.sleep(5000);
+
+		Assert.assertTrue(notificationSystem.getMessage().contains("All meals removed from Cart successfully"),
+				"[ERROR]: Meals removed message was not displayed.");
 	}
 }
--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -12,63 +12,76 @@ import pages.LoginPage;
 import pages.NotificationSystemPage;
 import pages.ProfilePage;
 
-public class ProfileTest extends BasicTest{
+public class ProfileTest extends BasicTest {
 
-	@Test(priority=0)
+	@Test(priority = 0)
 	public void editProfileTest() throws InterruptedException {
 		this.driver.navigate().to(this.baseUrl + "/guest-user/login-form");
-		
+		// Import Pages
 		LocationPopupPage locationPopup = new LocationPopupPage(driver, wait, js);
 		LoginPage login = new LoginPage(driver, wait, js);
 		NotificationSystemPage notificationSystem = new NotificationSystemPage(driver, wait, js);
 		ProfilePage profile = new ProfilePage(driver, wait, js);
 		AuthPage auth = new AuthPage(driver, wait, js);
-		
-		
+
+		// Login
 		locationPopup.closePopup();
 		login.login(email, password);
-		Assert.assertTrue(notificationSystem.getMessage().contains("Login Successfull"));
-		
-		
+		Assert.assertTrue(notificationSystem.getMessage().contains("Login Successfull"),
+				"[ERROR]: Login message was not displayed.");
+
+		// Update Profile
 		this.driver.navigate().to(this.baseUrl + "/member/profile");
 		profile.updateProfile("Will", "W", "Serbia", "056482456", "12500", "United Kingdom", "Cambridge", "Cambridge");
-		Assert.assertTrue(notificationSystem.getMessage().contains("Setup Successful"));
+		Assert.assertTrue(notificationSystem.getMessage().contains("Setup Successful"),
+				"[ERROR]: Setup successful message was not displayed.");
 		notificationSystem.notificationInvisible();
-		
+
+		// Logout
 		auth.logout();
-		Assert.assertTrue(notificationSystem.getMessage().contains("Logout Successfull!"));
-			
+		Assert.assertTrue(notificationSystem.getMessage().contains("Logout Successfull!"),
+				"[ERROR]: Logout message was not displayed.");
+
 	}
-	@Test(priority=5)
+
+	@Test(priority = 5)
 	public void changeProfileImageTest() throws InterruptedException, IOException {
 		this.driver.navigate().to(this.baseUrl + "/guest-user/login-form");
-	
+
+		// Import Pages
 		LocationPopupPage locationPopup = new LocationPopupPage(driver, wait, js);
 		LoginPage login = new LoginPage(driver, wait, js);
 		NotificationSystemPage notificationSystem = new NotificationSystemPage(driver, wait, js);
 		ProfilePage profile = new ProfilePage(driver, wait, js);
 		AuthPage auth = new AuthPage(driver, wait, js);
-		
+
+		// Login
+		locationPopup.closePopup();
 		login.login(email, password);
-		Assert.assertTrue(notificationSystem.getMessage().contains("Login Successfull"));
-		
+		Assert.assertTrue(notificationSystem.getMessage().contains("Login Successfull"),
+				"[ERROR]: Login message was not displayed.");
+
+		// Upload profile image
 		this.driver.navigate().to(this.baseUrl + "/member/profile");
-		
 		Thread.sleep(3000);
 		String imgPath = new File("images/slika.png").getCanonicalPath();
-		
+
 		profile.uploadImage(imgPath);
-		Assert.assertTrue(notificationSystem.getMessage().contains("Profile Image Uploaded Successfully"));
-		
+		Assert.assertTrue(notificationSystem.getMessage().contains("Profile Image Uploaded Successfully"),
+				"[ERROR]: Upload successfully message was not displayed.");
 		notificationSystem.notificationInvisible();
-		
-		profile.deleteImage();		
-		Assert.assertTrue(notificationSystem.getMessage().contains("Profile Image Deleted Successfully"));
-		
+
+		// Delete image
+		profile.deleteImage();
+		Assert.assertTrue(notificationSystem.getMessage().contains("Profile Image Deleted Successfully"),
+				"[ERROR]: Image Deleted message was not displayed.");
+
 		notificationSystem.notificationInvisible();
-		
+
+		// Logout
 		auth.logout();
-		Assert.assertTrue(notificationSystem.getMessage().contains("Logout Successfull!"));
+		Assert.assertTrue(notificationSystem.getMessage().contains("Logout Successfull!"),
+				"[ERROR]: Logout message was not displayed.");
 	}
-	
+
 }
